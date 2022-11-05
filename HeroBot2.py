82a00abb600e3224c1d7ac89ae12d8a53132f00b
viccsi <108951838+viccsi@users.noreply.github.com>
2022-11-05 15:51:16 +0100

    Update HeroBot2.py

--- HeroBot2.py
+++ HeroBot2.py
@@ -87,7 +87,7 @@ async def profil(ctx, member:discord.Member=None):
     embed.set_author(name="PROFIL " f"{name}" " 👤")
     embed.add_field(name="Alter", value=f"{cur_alter}", inline=False)
     embed.add_field(name="Idole", value=f"{cur_idol}", inline=False)
-    embed.add_field(name="Expérience", value="Level 1 -> f"{cur_xp}"" /5🔅", inline=False)
+    embed.add_field(name="Expérience", value="Level 1 -> f"{cur_xp}" "/5 🔅", inline=False)
     embed.add_field(name="Amitié", value=f"{cur_ami" "💫", inline=True)
     embed.add_field(name="Capacités", value=f"{cur_c_f}" " ⭐ Force\n" f"{cur_c_v}" " ⭐ Vitesse\n" f"{cur_c_s}" " ⭐ Stratégie\n" f"{cur_c_p}" " ⭐ Pouvoir\n" f"{cur_c_c}" " ⭐ Courage", inline=False)
     embed.add_field(name="Techniques spéciales", value=f"{cur_capa}", inline=True)
